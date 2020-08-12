3914282766ebeb600cda82c8cc45bc5f4ba21fab
Jorge Romero <>
2020-08-12 14:08:15 -0500

    adding resources to master

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+//use children you can do alot of  simple things and some props
+ 
+//if you want to always ensure structure always a property component with props  
+ReactDOM.render(<App/>, document.getElementById("root"))
